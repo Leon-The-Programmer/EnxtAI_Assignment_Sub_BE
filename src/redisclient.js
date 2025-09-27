d809a63524228cd755435f1aa8cd02f521b128ae
Leon <110422370+Leon-The-Programmer@users.noreply.github.com>
2025-09-28 03:48:23 +0530

    Modified Redis Client Code.
    This is to adapt to the Redis URL generated by Render. This is just a trial, if it doesn't work, more tweaks may require.

--- src/redisclient.js
+++ src/redisclient.js
@@ -1,17 +1,15 @@
 import { Redis } from "ioredis";
 
+const REDIS_URL = process.env.REDIS_URL;
 const REDIS_HOST = process.env.REDIS_HOST || "127.0.0.1";
 const REDIS_PORT = parseInt(process.env.REDIS_PORT || "6379");
 
-export const redis = new Redis({
-    host: REDIS_HOST,
-    port: REDIS_PORT
-});
+export const redis = REDIS_URL
+  ? new Redis(REDIS_URL)    // Use full URL if provided via environment variable..
+  : new Redis({             // Fallback for local dev if not..
+      host: REDIS_HOST,
+      port: REDIS_PORT
+    });
 
-redis.on("success", () => {
-    console.log("Redis is connected!");
-});
-
-redis.on("error", (error) => {
-    console.log("Error connecting to Redis due to ", error);
-});
+redis.on("connect", () => console.log("Redis is connected!"));
+redis.on("error", (err) => console.log("Error connecting to Redis due to", err));
